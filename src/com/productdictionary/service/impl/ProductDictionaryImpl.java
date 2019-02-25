5bce7b7bbebd0c56c0b284af6ca72911faf1c50c
sivendra savran <sivendra@itemmaster.com>
2019-02-25 11:06:36 +0530

    Added implementation for set dictionary entry in method set

--- src/com/productdictionary/service/impl/ProductDictionaryImpl.java
+++ src/com/productdictionary/service/impl/ProductDictionaryImpl.java
@@ -1,19 +1,53 @@
 package com.productdictionary.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.productdictionary.models.DictonaryEntry;
 import com.productdictionary.service.ProductDictionary;
 
 public class ProductDictionaryImpl implements ProductDictionary{
-
+    
+    private DictonaryEntry[] buckets;
+    private static final int DEFAULT_SIZE = 16;
+    private int size = 0;
+    
+    Map<String,Integer> map = new HashMap<>();
+    
+    
+    
+    public ProductDictionaryImpl() {
+        buckets = new DictonaryEntry[DEFAULT_SIZE];
+    }
+    
+    public ProductDictionaryImpl(int size) {
+        buckets = new DictonaryEntry[size];
+    }
+    
     @Override
     public void set(int key, String value) {
-        // TODO Auto-generated method stub
-        
+        DictonaryEntry dictonaryEntry = new DictonaryEntry(key, value);
+        int bucketIndex = getHashCode(key)%buckets.length;
+        DictonaryEntry existingDictonaryEntry = buckets[bucketIndex];
+        if(existingDictonaryEntry==null) {
+            buckets[bucketIndex] = dictonaryEntry;
+        } else {
+            while(existingDictonaryEntry!=null) {
+                if(existingDictonaryEntry.getKey()==key) {
+                    existingDictonaryEntry.setValue(value);
+                }
+                existingDictonaryEntry = existingDictonaryEntry.getNextEntry();
+            }
+        }
     }
 
     @Override
     public String get(int key) {
-        // TODO Auto-generated method stub
         return null;
     }
-
+    
+    public int getHashCode(int key) {
+        return Integer.valueOf(key).hashCode();
+    }
+    
 }
